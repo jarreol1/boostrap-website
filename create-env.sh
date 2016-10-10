22879246ca408fed5c83eef6eef6b87539cbe701
jarreol1 <jarreol1@hawk.iit.edu>
2016-10-10 00:25:19 +0000

    Modified create-env.sh and destroy-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+aws ec2 run-instances --image-id $1 --key-name hw3 --security-group-ids sg-e6f4219f --instance-type t2.micro --user-data file://installapp.sh --count 3
+
+echo
+
+echo “Sleeping for 30 seconds while instances are being run”
+
+sleep 30
+
+ID=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text`
+
+aws elb create-load-balancer --load-balancer-name itmo-444-jaa --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-ad4077c8
+
+aws elb register-instances-with-load-balancer --load-balancer-name itmo-444-jaa --instances $ID
+
+aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id ami-06b94666 --key-name hw3 --instance-type t2.micro --user-data file://installapp.sh
+
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name webserverdemo --launch-configuration webserver --availability-zone us-west-2b --load-balancer-name itmo-444-jaa --max-size 5 --min-size 0 --desired-capacity 1
+
+
+
--- destroy-env.sh
+++ destroy-env.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+AS=`aws autoscaling describe-auto-scaling-groups | awk '{print $3}' | head -1`
+
+ASLC=`aws autoscaling describe-auto-scaling-groups | awk '{print $9}' | head -1`
+
+LB=`aws elb describe-load-balancers | awk '{print $6}' | head -1`
+
+ILB=`aws elb describe-load-balancers | tail -n +3 | awk '{print $2}' | grep "i-*"`
+
+ID=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text`
+
+aws autoscaling detach-load-balancers --load-balancer-names $LB --auto-scaling-group-name $AS
+
+aws elb deregister-instances-from-load-balancer --load-balancer-name $LB --instances $ILB
+
+aws elb delete-load-balancer-listeners --load-balancer-name $LB --load-balancer-ports 80
+
+aws elb delete-load-balancer --load-balancer-name $LB
+
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $AS --force-delete
+
+aws autoscaling delete-launch-configuration --launch-configuration-name $ASLC
+
+aws ec2 terminate-instances --instance-ids $ID
