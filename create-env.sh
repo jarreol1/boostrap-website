965a379348c173beeab389103b0cd413131f0ff2
jarreol1 <jarreol1@hawk.iit.edu>
2016-10-10 22:43:30 +0000

    Added client-token, working well now

--- create-env.sh
+++ create-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --key-name hw4 --security-group-ids sg-8474f0fd --instance-type t2.micro --user-data file://installapp.sh --count 3
+aws ec2 run-instances --image-id $1 --key-name hw4 --security-group-ids sg-8474f0fd --instance-type t2.micro --client-token current-running-instances --user-data file://installapp.sh --count 3
 
 echo
 
@@ -8,7 +8,7 @@ echo “Sleeping for 30 seconds while instances are being run”
 
 sleep 30
 
-ID=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --filters Name=instance-state-name,Values=running --output text`
+ID=`aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId,ClientToken]' | grep current-running-instances | awk '{print $1}'`
 
 aws elb create-load-balancer --load-balancer-name itmo-444-jaa --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-ad4077c8
 
