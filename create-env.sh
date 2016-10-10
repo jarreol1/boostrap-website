33fc165c60a1eb98cc63d0a7ad5f824297437371
jarreol1 <jarreol1@hawk.iit.edu>
2016-10-10 02:51:07 +0000

    Editing the create and destroy scripts

--- create-env.sh
+++ create-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --key-name hw3 --security-group-ids sg-e6f4219f --instance-type t2.micro --user-data file://installapp.sh --count 3
+aws ec2 run-instances --image-id $1 --key-name hw4 --security-group-ids sg-8474f0fd --instance-type t2.micro --client-token first-run --user-data file://installapp.sh --count 3
 
 echo
 
@@ -17,6 +17,3 @@ aws elb register-instances-with-load-balancer --load-balancer-name itmo-444-jaa 
 aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id ami-06b94666 --key-name hw3 --instance-type t2.micro --user-data file://installapp.sh
 
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name webserverdemo --launch-configuration webserver --availability-zone us-west-2b --load-balancer-name itmo-444-jaa --max-size 5 --min-size 0 --desired-capacity 1
-
-
-
